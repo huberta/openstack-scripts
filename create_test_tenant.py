1f0082588fe3b01450ce156396aaf6059e23c55d
Kamil Madac <kamil.madac@gmail.com>
2013-10-14 23:02:30 +0200

    Added creation of private network Password as scrip argument

--- create_test_tenant.py
+++ create_test_tenant.py
@@ -2,10 +2,13 @@ __author__ = 'kmadac'
 
 import keystoneclient.v2_0.client as ksclient
 import keystoneclient.apiclient.exceptions
+import neutronclient.v2_0.client as nclient
+import neutronclient.common.exceptions
 
 import argparse
 
 endpoint = "http://192.168.122.200:35357/v2.0"
+keystone_url = "http://192.168.122.200:5000/v2.0"
 admin_token = "ADMIN"
 
 
@@ -26,7 +29,7 @@ def create_user(keystone, useremail, password, tenant, role_name='_member_'):
         new_user = keystone.users.create(name=useremail, password=password, email=useremail, tenant_id=tenant.id)
     except keystoneclient.apiclient.exceptions.Conflict:
         print "User {0} already exists".format(useremail)
-        new_user = keystone.tenants.find(name=useremail)
+        new_user = keystone.users.find(name=useremail)
 
     member_role = keystone.roles.find(name=role_name)
 
@@ -45,15 +48,55 @@ def create_user(keystone, useremail, password, tenant, role_name='_member_'):
     return new_user
 
 
+def create_internal_network(neutron, network_name='private_network', network_address='192.168.0.0/24'):
+    neutron.format = 'json'
+
+    networks = neutron.list_networks()
+    network = None
+
+    # check whether private network exists
+    for net in networks['networks']:
+        if net['name'] == network_name:
+            network = net
+            print "Network {0} already exists".format(network_name)
+            break
+
+    if not network:
+        networkdict = {'name': network_name, 'admin_state_up': True}
+        network = neutron.create_network({'network': networkdict})
+
+    subnet = None
+    try:
+        subnet = neutron.create_subnet({'subnet': {'name': 'sub_'+network_name,
+                                        'network_id': network['id'],
+                                        'ip_version': 4,
+                                        'cidr': network_address}})
+    except neutronclient.common.exceptions.NeutronClientException:
+        print "Subnet {0} already exists".format(network_address)
+
+    return network, subnet
+
+
 def main():
     parser = argparse.ArgumentParser(description='Script for creating testing tenant')
     parser.add_argument('user_email', action="store", help="User email will be used as name of tenant")
+    parser.add_argument('password', action="store", help="Password for user", default='Start123')
 
     args = parser.parse_args()
 
     keystone = ksclient.Client(token=admin_token, endpoint=endpoint)
     new_tenant = create_tenant(keystone, args.user_email)
-    create_user(keystone, args.user_email, password='lol', tenant=new_tenant)
+    create_user(keystone, args.user_email, password=args.password, tenant=new_tenant)
+
+    admin_user = keystone.users.find(name='admin')
+
+    neutron = nclient.Client(username='admin',
+                             password='admin_pass',
+                             tenant_name=new_tenant.name,
+                             auth_url=keystone_url)
+    create_internal_network(neutron)
+
+
 
 if __name__ == "__main__":
     main()
