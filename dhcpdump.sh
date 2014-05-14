57caf65a3f2ffa736a5654e344f6b4fd2dfe5d7d
Kamil Madac <kamil.madac@gmail.com>
2014-05-14 11:52:35 +0200

    Fixed which enables to find correct ports and interfaces only by fixed ip and tenant name Hostname detection implemeted via API instead of Mysql

--- dhcpdump.sh
+++ dhcpdump.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
 GREP=/bin/grep
 AWK=/usr/bin/awk
+CUT=/usr/bin/cut
 QUANTUM=/usr/bin/quantum
+KEYSTONE=/usr/bin/keystone
+NOVA=/usr/bin/nova
 MULTITAIL=/usr/bin/multitail
 IP=/sbin/ip
-MYSQL=/usr/bin/mysql
 TCPDUMP="/usr/sbin/tcpdump -ln -e"
 TCPDUMPFILTER="'(port bootps) or icmp'"
-LOCALHOSTNAME=`/bin/hostname`
+LOCALHOSTNAME=`/bin/hostname -f`
 GAWK=/usr/bin/gawk
 
-VMNAME=$1
-INTERNALIP=$2
+INTERNALIP=$1
+TENANT_NAME=$2
 
-NOVADBUSER=`$GAWK 'match($0,/mysql:\/\/(.*):(.*)@(.*)\//, a) {print a[1]}' /etc/nova/nova.conf`
-NOVADBPASS=`$GAWK 'match($0, /mysql:\/\/(.*):(.*)@(.*)\//, a) {print a[2]}' /etc/nova/nova.conf`
-DBHOST=`$GAWK 'match($0, /mysql:\/\/(.*):(.*)@(.*)\//, a) {print a[3]}' /etc/nova/nova.conf`
+# find tenant id
+TENANT_ID=`$KEYSTONE tenant-list | $GREP $TENANT_NAME | $AWK -F" | " '{print $2}'`
 
-if [[ -z "$NOVADBUSER" ]]; then
-  echo "Error: DB user for nova not found!!!"
+if [[ -z "$TENANT_ID" ]]; then
+  echo "Error: Tenant ID not found for $TENANT_NAME!!!"
   exit 1
 fi
 
-HYPERVISORNAME=`$MYSQL -h $DBHOST -u$NOVADBUSER -p$NOVADBPASS -B -D nova -e "select host from instances where display_name='$VMNAME';" | grep -v host`
+# get all ports from quantum filtered by fixed IP
+PORTS_WITH_IP=`$QUANTUM port-list | grep $INTERNALIP | $AWK -F" | " '{print $2}'`
+
+# find VM name for which has specified fixed IP and belongs to specified tenant
+for PORT in $PORTS_WITH_IP;
+do
+  PORT_INFO=`$QUANTUM port-show $PORT | $AWK '/ device_id / { print $4 } / id / { print $4 } / tenant_id / { print $4 } / network_id / { print $4 }'`
+  VM_ID=`echo $PORT_INFO | $CUT -d" " -f 1`
+  INT_SUFFIX=`echo $PORT_INFO | $AWK '{print substr($2, 0, 11)}'`
+  NET_ID=`echo $PORT_INFO | $CUT -d" " -f 3`
+  if [[ $PORT_INFO == *$TENANT_ID* ]]; then
+    VM_INFO=`$NOVA show $VM_ID | $AWK '/ OS-EXT-SRV-ATTR:hypervisor_hostname / { print $4 } / name / { print $4 }'`
+    HYPERVISORNAME=`echo $VM_INFO | $CUT -d" " -f 1`
+    VM_NAME=`echo $VM_INFO | $CUT -d" " -f 2`
+    break
+  fi
+done
+
+if [[ -z "$VM_NAME" ]]; then
+  echo "Error: VM Name not found!!!"
+  exit 1
+fi
 
 if [[ -z "$HYPERVISORNAME" ]]; then
-  echo "Error: Hypervisor name not found for $VMNAME!!!"
+  echo "Error: Hypervisor name not found for $VM_NAME!!!"
   exit 1
 fi
 
-INT_SUFFIX=`$QUANTUM port-list |  $GREP $INTERNALIP | $AWK -F" | " '{print substr($2, 0, 11)}'`
-SUBNET_ID=`$QUANTUM port-list |  $GREP $INTERNALIP | $AWK -F": " '{print substr($2, 2, 36)}'`
-NET_ID=`$QUANTUM net-list | $GREP $SUBNET_ID | $AWK -F" | " '{print substr($2,1,36)}'`
+if [[ -z "$INT_SUFFIX" ]]; then
+  echo "Error: Cannot determine interface suffix for IP $INTERNALIP!!!"
+  exit 1
+fi
+
 DHCP_TAP=`$IP netns exec qdhcp-$NET_ID $IP a | $GREP inet | $GREP tap | $AWK -F" " '{print $7}'`
 
 if [[ -z "$DHCP_TAP" ]]; then
@@ -39,14 +63,14 @@ if [[ -z "$DHCP_TAP" ]]; then
   exit 1
 fi
 
-echo "VMname = $VMNAME"
+echo "VMname = $VM_NAME"
 echo "Hypervisor = $HYPERVISORNAME"
 echo "Interface suffix = $INT_SUFFIX"
-echo "Subnet = $SUBNET_ID"
+#echo "Subnet = $SUBNET_ID"
 echo "Net = $NET_ID"
 echo "DHCP Tap = $DHCP_TAP"
 
-if [ $LOCALHOSTNAME != $HYPERVISORNAME ]; then
+if [[ "$LOCALHOSTNAME" != "$HYPERVISORNAME" ]]; then
   SSH="ssh root@$HYPERVISORNAME"
   echo "VM on remote host"
 fi
